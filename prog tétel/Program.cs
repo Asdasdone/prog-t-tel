dfca468bc5eec2a13a64377dd57ce9b97a9bb03c
Asdasdone <tohm69@gmail.com>
2020-09-11 09:51:39 +0200

    Add project files.

---
+++ prog tétel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_tétel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
